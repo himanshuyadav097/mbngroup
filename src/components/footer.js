ce3fa6b7d89b81014a8952931ef390c04abab7a0
Your Name <you@example.com>
2020-05-14 09:36:47 +0530

    commit

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const footer=()=>{
+
+    return(
+
+     
+        <div class="footer">
+        <p>Footer</p>
+      </div>
+      
+    )
+
+}
+
+export default footer;
---
+++ src/menu.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+ const Menu = () => {
+
+    return (
+        <div className="topnav">
+  <ul >
+  <li><a href="/">Home</a></li>
+  <li><a href="news">Admissions Process</a></li>
+  <li><a href="news">Learning</a></li>
+  <li><a href="aboutus">About Us</a></li>
+  <li><a href="news">Gallery</a></li>
+  <li><a href="contactus">Contact Us</a></li>
+  </ul>
+    </div>
+
+    )
+  
+ }
+
+ export default Menu;
